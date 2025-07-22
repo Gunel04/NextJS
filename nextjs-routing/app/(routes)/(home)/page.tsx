7c02779a77f43833d082e9c33d27418aac7f31af
Gunel04 <alkhasovagunel@gmail.com>
2025-07-22 19:22:25 +0400

    use-cache

--- nextjs-routing/app/(routes)/(home)/page.tsx
+++ nextjs-routing/app/(routes)/(home)/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import type { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Home',
   description: 'Home page',
 }
 const Home = () => {
   return (
-    <div>Home page</div>
+    <>
+      <div>Home page</div>
+      <input type="text" placeholder='Name' className='border-1' />
+    </>
   )
 }
 
--- nextjs-routing/app/(routes)/about/page.tsx
+++ nextjs-routing/app/(routes)/about/page.tsx
@@ -1,13 +1,18 @@
+'use client' 
 import React from 'react'
 import type { Metadata } from 'next'
- 
+import { usePathname } from 'next/navigation'
 export const metadata: Metadata = {
   title: 'About',
   description: '...',
 }
 const About = () => {
+  const path = usePathname();
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+      <p>{path}</p>
+    </div>
   )
 }
 
